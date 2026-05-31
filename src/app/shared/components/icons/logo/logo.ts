a52791d09554ff5eb9c139ae135845d0477efff2
Teodora <teodora.chiosa@gmail.com>
2026-05-31 18:00:12 +0300

    Update logo

--- src/app/shared/components/icons/logo/logo.ts
+++ src/app/shared/components/icons/logo/logo.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CurrentRouteService } from '@shared/services/current-route.service';
 
 @Component({
   selector: 'app-logo',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './logo.html',
   styleUrl: './logo.scss',
 })
 export class Logo {
+  currentRouteService = inject(CurrentRouteService);
+  currentRoute = '';
+
+  constructor() {
+    this.currentRoute = this.currentRouteService.getCurrentRoute();
+  }
 
 }
