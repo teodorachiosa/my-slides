96952149f4f7f9baadd9209e87a2ee5cbe914eba
Teodora <teodora.chiosa@gmail.com>
2026-06-16 17:24:25 +0300

    Remove unused function

--- src/app/shared/directives/slide-set/slide-set.ts
+++ src/app/shared/directives/slide-set/slide-set.ts
@@ -15,7 +15,7 @@ import { Subscription } from 'rxjs';
 import { AttachComponentService } from '@shared/services/attach-component.service';
 import { TranslatedSlide } from '@shared/models/translation.model';
 
-const SHAMEFUL_TIMEOUT = 300;
+const SHAMEFUL_TIMEOUT = 250;
 
 @Directive()
 export class SlideSet implements OnInit, AfterViewInit, OnDestroy {
@@ -31,20 +31,13 @@ export class SlideSet implements OnInit, AfterViewInit, OnDestroy {
   previousTranslationData: LangChangeEvent = { lang: '', translations: {} };
 
   ngOnInit(): void {
-    const baseTranslationRaw = this.translateService.instant(this.setName);
-    if (typeof baseTranslationRaw === 'string' && baseTranslationRaw === this.setName) {
-      return;
-    }
-
-    this.baseTranslation.set(baseTranslationRaw);
     this.translationsSubscription = this.translateService
       .stream(this.setName)
       .subscribe((newTranslationObject: Record<number, TranslatedSlide>) => {
         const newTranslation = Object.values(newTranslationObject);
 
         if (Array.isArray(newTranslation)) {
-          const mergedContent = this.mergeTranslations(this.baseTranslation(), newTranslation);
-          this.slidesContent.set(mergedContent);
+          this.slidesContent.set(newTranslation);
         }
       });
   }
@@ -81,27 +74,6 @@ export class SlideSet implements OnInit, AfterViewInit, OnDestroy {
       if (!this.destroyed()) {
         this.attachComponentService.attachComponent(component);
       }
-    });
-  }
-
-  mergeTranslations(
-    baseTranslation: TranslatedSlide[],
-    newTranslation: TranslatedSlide[],
-  ): TranslatedSlide[] {
-    let lastColorVariableUsed: string | undefined;
-
-    return newTranslation.map((newTranslationItem, index) => {
-      const baseColor = baseTranslation[index]?.backgroundColor;
-      const newColor = newTranslationItem.backgroundColor ?? baseColor ?? lastColorVariableUsed;
-
-      if (newColor) {
-        lastColorVariableUsed = newColor;
-      }
-
-      return {
-        backgroundColor: newColor ?? '',
-        content: newTranslationItem.content,
-      };
     });
   }
 }
